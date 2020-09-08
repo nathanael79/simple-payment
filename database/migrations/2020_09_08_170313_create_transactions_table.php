9b31270be2d2992299c6afb11a35ffdfc24d9153
Imanuel Ronaldo <imanuelronaldo@gmail.com>
2020-09-09 00:13:42 +0700

    initial commit

---
+++ database/migrations/2020_09_08_170313_create_transactions_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('user_id');
+            $table->bigInteger('amount');
+            $table->enum('type',['credit','debit']);
+            $table->bigInteger('balance_before');
+            $table->bigInteger('balance_after');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+}
